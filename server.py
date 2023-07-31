12316818961eadb07ed295c4eb3bee9654e05c14
zubairmh <zubair.mh@protonmail.com>
2023-07-31 23:25:38 +0530

    some files

--- server.py
+++ server.py
@@ -1,7 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
-app=Flask(__name__)
+global comments
+comments = []
+
+global counter
+counter=0
+app = Flask(__name__)
+
+
 @app.route("/")
-async def home():
+def home():
     return render_template("index.html")
-app.run()
+
+
+@app.route("/injection")
+def injection():
+    return render_template("injection.html", comments=comments)
+
+@app.route("/comment")
+def comment():
+    global comments
+    comments.append("X")
+    return "OK"
+
+@app.route("/ddos")
+def ddos():
+    global counter
+    counter+=1
+    return render_template("ddos.html", count=counter)
+
+@app.route("/calculate")
+def calculate():
+    number = int(request.args.get("number"))
+    power = int(request.args.get("power"))
+    return f"Power is: {number**power}"
+
+
+app.run(debug=True)
